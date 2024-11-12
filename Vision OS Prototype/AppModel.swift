99686db8cd9f0d3b6b01e128f6876d62f1314d46
Leon Schreiber <leon.schreiber@tu-berlin.de>
2024-11-12 16:30:17 +0100

    Initial Commit

---
+++ Vision OS Prototype/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Vision OS Prototype
+//
+//  Created by Leon Schreiber on 12.11.24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Vision OS Prototype/Vision_OS_PrototypeApp.swift
@@ -0,0 +1,33 @@
+//
+//  Vision_OS_PrototypeApp.swift
+//  Vision OS Prototype
+//
+//  Created by Leon Schreiber on 12.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Vision_OS_PrototypeApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
